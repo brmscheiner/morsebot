9bc5e8a291368afebf590ee96ba5f8ab29814fdd
ben <brmscheiner@gmail.com>
2016-04-18 23:04:43 -0400

    Reply to tweets with longshort or -. format

--- morsebot.py
+++ morsebot.py
@@ -1,5 +1,23 @@
-import tweepy,random,api_keys,topusers
+import random
+import tweepy
+import api_keys
 
+class MyStreamListener(tweepy.StreamListener):
+    ''' http://docs.tweepy.org/en/v3.4.0/streaming_how_to.html '''
+    
+    def on_status(self,data):
+        try:
+            text = data.text
+            replyid = data.id
+            username = data.user.screen_name
+            if '@morsehorse_bot' in text:
+                reply = buildTweets(text, username)
+                for tweet in reply:
+                    print(tweet)
+                    api.update_status(status=tweet, in_reply_to_status_id=replyid)
+        except UnicodeError:
+            pass
+                    
 def str2morse(x):
     morseDict = {'A': '.-',     'B': '-...',   'C': '-.-.', 
         'D': '-..',    'E': '.',      'F': '..-.',
@@ -24,41 +42,46 @@ def str2morse(x):
             #print(c+' was ignored.')
     return ans
             
-def twitterSetup():
+def morse2longshort(x):
+    ans = ''
+    for c in x:
+        if c=='-':
+            ans += 'LONG '
+        elif c=='.':
+            ans += 'SHORT '
+    return ans
+    
+def apiSetup():
 	CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET = api_keys.getKeys()
 	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 	api = tweepy.API(auth)
 	return api
 	
-def buildTweets(x,n):
-    ''' n is the number of spare characters '''
-    all = str2morse(x)+str2morse("you're welcome i love you lol")
+def buildTweets(x, username):
+    ''' Convert string x to a list of morse code tweets that tag @username '''
+    spaceLeft = 140 - len(username) - 3
+    if random.random()>0.5:
+        all = str2morse(x)+str2morse("you're welcome i love you lol")
+    else:
+        all = morse2longshort(str2morse(x))
     i = 0
     tweets = []
     for c in all:
-        if i%n == 0:
+        if i%spaceLeft == 0:
             tweets.append('')
-        tweets[len(tweets)-1] += c
+        tweets[-1] += c
         i += 1
+    tweets = [tweet+' @'+username for tweet in tweets]
     return tweets
     
 if __name__=='__main__':
-    api = twitterSetup()
-    # twt = api.search('the')
-    twt = api.search('@morsehorse_bot')
-    for s in twt:
-        try:
-            spaceLeft = 140 - len(s.user.screen_name) - 3
-            reply = buildTweets(s.text, spaceLeft)
-            print(s.text)
-            print(s.user.screen_name)
-            for each in reply:
-                api.update_status(each+' @'+s.user.screen_name)
-        except UnicodeEncodeError:
-            print('ignoring a tweet')
-        #str2morse('hello')
-
-
-
+    api = apiSetup()
+    horseListener = MyStreamListener()
+    horseStream = tweepy.Stream(auth=api.auth, listener=horseListener)
+    horseStream.userstream()
+    
+    
+    
+    
  
